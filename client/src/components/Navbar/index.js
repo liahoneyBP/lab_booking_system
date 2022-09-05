9c5c6474e6e5f66da3a5cd4091bd1db0edaf0097
BewSons <bewpinn55@gmail.com>
2022-09-05 17:02:18 +0700

    User Logout

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -1,51 +1,84 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Menu } from "antd";
 import {
-    Nav, 
-    NavLink, 
-    Bars, 
-    NavMenu, 
-    NavBtn, 
-    NavBtnLink} from './NavbarElements';
+  AppstoreOutlined,
+  SettingOutlined,
+  UserOutlined,
+  UserAddOutlined,
+  LogoutOutlined,
+} from "@ant-design/icons";
+import { Link } from "react-router-dom";
 
-const Navbar = () => {
+
+import { getAuth, signOut } from "firebase/auth";
+
+import { useDispatch } from "react-redux";
+import {useNavigate} from 'react-router-dom';
+
+const { SubMenu, Item } = Menu;
+
+
+const Header = () => {
+  const [current, setCurrent] = useState("home");
+
+  let dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const handleClick = (e) => {
+    console.log(e.key);
+    setCurrent(e.key);
+  };
+  
+
+  const logout = () => {
+    const auth = getAuth();
+    signOut(auth).then(() => {
+      // Sign-out successful.
+    }).catch((error) => {
+      // An error happened.
+    });
+    dispatch({
+      type: "LOGOUT",
+      payload: null,
+    });
+    navigate("/sign-in");
+  };
+
   return (
-    <>
-    <Nav>
-        <NavLink to="/">
-            <p>Logo</p>
-        </NavLink>
-        <Bars />
+    <Menu theme="dark" onClick={handleClick} selectedKeys={[current]} mode="horizontal">
+      <Item key="home" icon={<AppstoreOutlined />}>
+        <Link to="/">Home</Link>
+      </Item>
 
-        <NavMenu>
-            <NavLink 
-              to="/lab"
-            >
-                LAB
-            </NavLink>
-            <NavLink 
-              to="/mybookings" 
-              >
-                MY BOOKINGS
-            </NavLink>
-            <NavLink 
-              to="/schedule"
-              >
-                SCHEDULE
-            </NavLink>
-            <NavLink 
-              to="/sign-up" 
-              >
-                Sign Up
-            </NavLink>
-        </NavMenu>
-        <NavBtn>
-            <NavBtnLink to="/sign-in">
-                Sign In
-            </NavBtnLink>
-        </NavBtn>
-    </Nav>
-    </>
-  )
-}
+      <Item key="lab" icon={<AppstoreOutlined />}>
+        <Link to="/lab">Lab</Link>
+      </Item>
 
-export default Navbar
+      <Item key="mybookings" icon={<AppstoreOutlined />}>
+        <Link to="/mybookings">My Bookings</Link>
+      </Item>
+
+      <Item key="schedule" icon={<AppstoreOutlined />}>
+        <Link to="/schedule">Schedule</Link>
+      </Item>
+
+      <Item key="sign-up" icon={<UserAddOutlined />} className="float-right">
+        <Link to="/sign-up">Sign Up</Link>
+      </Item>
+
+      <Item key="sign-in" icon={<UserOutlined />} className="float-right">
+        <Link to="/sign-in">Sign In</Link>
+      </Item>
+
+      <SubMenu icon={<SettingOutlined />} title="Username">
+        <Item key="setting:1">Option 1</Item>
+        <Item key="setting:2">Option 2</Item>
+        <Item icon={<LogoutOutlined />} onClick={logout}>
+          Logout
+        </Item>
+      </SubMenu>
+    </Menu>
+  );
+};
+
+export default Header;
