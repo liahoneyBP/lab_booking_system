abfa56b486f82e3d4b32e773aff2a0f80affadba
BewSons <bewpinn55@gmail.com>
2022-09-23 19:12:19 +0700

    Admin Nav Bar

---
+++ client/src/components/Navbar/AdminNav.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const AdminNav = () => (
+  <nav>
+    <ul className="nav flex-column">
+      <li className="nav-item">
+        <Link to="/admin/dashboard" className="nav-link">
+          Dashboard
+        </Link>
+      </li>
+
+      <li className="nav-item">
+        <Link to="/admin/lab" className="nav-link">
+          Lab
+        </Link>
+      </li>
+
+      <li className="nav-item">
+        <Link to="/admin/labs" className="nav-link">
+          Labs
+        </Link>
+      </li>
+
+      
+    </ul>
+  </nav>
+);
+
+export default AdminNav;
