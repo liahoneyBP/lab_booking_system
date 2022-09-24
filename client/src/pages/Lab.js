e7bdd5fcc234463f842aef21478638f77271d781
BewSons <bewpinn55@gmail.com>
2022-09-25 04:26:30 +0700

    Search Redux Setup

--- client/src/pages/Lab.js
+++ client/src/pages/Lab.js
@@ -1,18 +1,27 @@
 import React from "react";
+import Search from "../components/forms/Search";
+import Jumbotron from "../components/cards/Jumbotron";
+
 
 const Lab = () => {
     return (
-        <div
-            style={{
-                display: 'flex',
-                justifyContent: 'center',
-                alignItems: 'center',
-                height: '90vh'
-            }}
-            >
-                <h1>Lab Page Krub</h1>
-        </div>
-    )
+        <>
+          <div className="jumbotron text-primary h1 font-weight-bold text-center">
+            <Jumbotron text={["LAB PAGE"]} />
+          </div>
+    
+          <div className="container">
+            <div className="row">
+                <div className="col md-1">
+              
+                    <Search />
+               
+                </div>
+                
+            </div>
+          </div>
+        </>
+      );
 }
 
 export default Lab;
