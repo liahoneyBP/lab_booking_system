51735295a4a7acb9d6f7ba393b81f379c0c69936
BewSons <bewpinn55@gmail.com>
2022-09-25 01:26:15 +0700

    Lab Card Component

---
+++ client/src/components/cards/LabCard.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const LabCard = ({lab}) => 
+    <div>
+        {lab.labName}
+    </div>
+
+
+export default LabCard;
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,5 +1,6 @@
 import React, {useEffect, useState} from "react";
 import { getLabsByCount } from "../functions/lab";
+import LabCard from "../components/cards/LabCard"
 
 const Home = () => {
     const [labs, setLabs] = useState([])
@@ -10,19 +11,33 @@ const Home = () => {
     }, [])
 
     const loadAllLabs = () => {
+        setLoading(true)
         getLabsByCount(3)
         .then(res => {
             setLabs(res.data);
+            setLoading(false);
         })
     }
 
 
     return (
-        <div>
-            <p>React Home</p>
-            {JSON.stringify(labs)}
+        <>
+        <div className="jumbotron">
+            {loading ? (<h4>Loading...</h4>) : <h4>All Labs</h4>}
         </div>
-    );
+
+        <div className="container">
+            <div className="row">
+               {labs.map((lab) => (
+                <div key={lab._id} className="col-md-4">
+                    <LabCard lab={lab} />
+                </div>
+               ))}
+            </div>
+        </div>
+        </>
+    
+    )
 }
 
 export default Home;
