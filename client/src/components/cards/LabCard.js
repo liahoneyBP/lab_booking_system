b6cd3c7fbb11b5421d98ac0367b0fe2ec61b3c51
BewSons <bewpinn55@gmail.com>
2022-09-25 01:49:21 +0700

    Using Ant Design Card

--- client/src/components/cards/LabCard.js
+++ client/src/components/cards/LabCard.js
@@ -1,9 +1,36 @@
 import React from 'react'
+import {Card} from 'antd'
+import {EyeOutlined, ShoppingCartOutlined} from '@ant-design/icons'
+import LabImgDefault from "../../images/LabUtcc_Default.jpg"
+import { Link } from "react-router-dom"
 
-const LabCard = ({lab}) => 
-    <div>
-        {lab.labName}
-    </div>
+const { Meta } = Card;
 
+const LabCard = ({ lab }) => {
+    // destructure
+    const { images, labName, details, slug } = lab;
+    return (
+      <Card
+        cover={
+          <img
+            src={images && images.length ? images[0].url : LabImgDefault}
+            style={{ height: "150px", objectFit: "cover" }}
+            className="p-1"
+          />
+        }
+        actions={[
+          <Link to={`/lab/${slug}`}>
+            <EyeOutlined className="text-warning" /> <br /> View Lab
+          </Link>,
+          
+        ]}
+      >
+        <Meta
+          title={labName}
+          description={`${details && details.substring(0, 40)}...`}
+        />
+      </Card>
+    );
+  };
 
 export default LabCard;
