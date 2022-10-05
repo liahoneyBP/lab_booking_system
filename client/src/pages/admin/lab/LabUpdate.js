4f0781332662e92d38268fd509b23a06b24bec09
BewSons <bewpinn55@gmail.com>
2022-10-05 21:43:06 +0700

    Get Lab on Update Page

--- client/src/pages/admin/lab/LabUpdate.js
+++ client/src/pages/admin/lab/LabUpdate.js
@@ -2,17 +2,43 @@ import React, { useState, useEffect } from "react";
 import AdminNav from "../../../components/Navbar/AdminNav";
 import { toast } from "react-toastify";
 import { useSelector } from "react-redux";
-import { createLab } from "../../../functions/lab";
-import LabCreateForm from "../../../components/forms/LabCreateForm";
+import { getLab } from "../../../functions/lab";
 import FileUpload from "../../../components/forms/FileUpload";
 import { LoadingOutlined } from "@ant-design/icons";
 
 import { useParams } from 'react-router-dom';
 
+
+const initialState = {
+  labName: "15201QQ",
+  building: "5",
+  details: "ห้องแล็บ Test",
+  floor: "5",
+  capacity: "50",
+  images: [],
+  equipment: {},
+  bookings: [],
+}
+
+
 const LabUpdate = () => {
+
+  const [values, setValues] = useState(initialState);
   const { user } = useSelector((state) => ({ ...state }));
   // router
   const { slug } = useParams()
+
+  useEffect(() => {
+    loadLab()
+  }, [])
+
+  const loadLab = () => {
+    getLab(slug)
+    .then(lab => {
+    //   console.log('single lab', lab);
+     setValues({...values, ...lab.data});  
+  })
+  }
 
   return (
     <div className="container-fluid">
@@ -23,7 +49,7 @@ const LabUpdate = () => {
 
         <div className="col-md-10">
           <h4>Product update</h4>
-          {JSON.stringify(slug)}
+          {JSON.stringify(values)}
           <hr />
         </div>
       </div>
